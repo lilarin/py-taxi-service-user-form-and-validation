31795063a2142205c4ba31f2f6559cfa88c09f9d
lilarin <80650911+lilarin@users.noreply.github.com>
2024-07-18 11:57:52 +0300

    Solution refinement

--- taxi/urls.py
+++ taxi/urls.py
@@ -16,15 +16,19 @@ from .views import (
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
-    toggle_car_assign_view
+    toggle_car_assign_view,
 )
 
 urlpatterns = [
-    path("", index, name="index"),
+    path(
+        "",
+        index,
+        name="index"
+    ),
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
-        name="manufacturer-list",
+        name="manufacturer-list"
     ),
     path(
         "manufacturers/create/",
@@ -41,12 +45,41 @@ urlpatterns = [
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete",
     ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("cars/create/", CarCreateView.as_view(), name="car-create"),
-    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path(
+        "cars/<int:pk>/toggle/",
+        toggle_car_assign_view,
+        name="car-toggle"
+    ),
+    path(
+        "cars/",
+        CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "cars/create/",
+        CarCreateView.as_view(),
+        name="car-create"
+    ),
+    path(
+        "cars/<int:pk>/update/",
+        CarUpdateView.as_view(),
+        name="car-update"
+    ),
+    path(
+        "cars/<int:pk>/delete/",
+        CarDeleteView.as_view(),
+        name="car-delete"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
     path(
         "drivers/<int:pk>/",
         DriverDetailView.as_view(),
@@ -65,12 +98,7 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/update/",
         DriverUpdateLicenseView.as_view(),
-        name="driver-update"
-    ),
-    path(
-        "cars/<int:pk>/toggle/",
-        toggle_car_assign_view,
-        name="car-toggle",
+        name="driver-update",
     ),
 ]
 
--- taxi/views.py
+++ taxi/views.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarCreationForm
-from .models import Driver, Car, Manufacturer
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseRedirect
+)
+from .forms import (
+    DriverCreationForm,
+    DriverLicenseUpdateForm,
+    CarCreationForm
+)
+from .models import (
+    Driver,
+    Car,
+    Manufacturer
+)
 
 
 @login_required
@@ -88,7 +99,7 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
 
 
 class DriverCreateView(LoginRequiredMixin, generic.CreateView):
@@ -110,7 +121,7 @@ class DriverUpdateLicenseView(LoginRequiredMixin, generic.UpdateView):
 @login_required()
 def toggle_car_assign_view(request: HttpRequest, pk: int) -> HttpResponse:
     driver = Driver.objects.get(pk=request.user.id)
-    if Car.objects.get(pk=pk) in driver.cars.all():
+    if Car.objects.get_object_or_404(pk=pk) in driver.cars.all():
         driver.cars.remove(pk)
     else:
         driver.cars.add(pk)
